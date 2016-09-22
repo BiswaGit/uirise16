f34020f29f38c9fa0f9b21b3c38b06c062243b14
Biswaranjan Behera <biswaranjan.b@outlook.com>
2016-09-22 18:09:26 +0530

    Rise Hackathon UI

---
+++ app/landing/card.js
@@ -0,0 +1,11 @@
+/* Defines the card entity */
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[],
+        execute: function() {
+        }
+    }
+});
+//# sourceMappingURL=card.js.map
---
+++ app/main.ts
@@ -0,0 +1,10 @@
+import { bootstrap }    from 'angular2/platform/browser';
+import { ROUTER_PROVIDERS } from 'angular2/router';
+
+
+// Our main component
+import { AppComponent } from './app.component';
+import {enableProdMode} from 'angular2/core';
+
+enableProdMode();
+bootstrap(AppComponent, [ROUTER_PROVIDERS]);
