b5d1afbbfdf6c935ecc56f669d5884b1078ed1f5
aarahan7 <aarahan7@gmail.com>
2016-09-25 10:15:54 +0530

    otp

--- app/payamount/qrcodeacript.js
+++ app/payamount/qrcodeacript.js
@@ -7,7 +7,7 @@ function sendOTP(){
         type: "GET",
         async: "false",
         dataType:'text',
-        url: 'http://f65bc691.ngrok.io/saralpay/send-otp',
+        url: 'http://52.193.0.37:8080/saralpay/send-otp',
         success: function(data) {
         	console.log('html' + data);
         	 
