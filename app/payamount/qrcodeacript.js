ca045be070ccdbf269b253fe06e5ef758b87b76c
BiswaGit <biswaranjan.b@outlook.com>
2016-10-19 15:07:55 +0530

    Update qrcodeacript.js

--- app/payamount/qrcodeacript.js
+++ app/payamount/qrcodeacript.js
@@ -7,7 +7,7 @@ function sendOTP(){
         type: "GET",
         async: "false",
         dataType:'text',
-        url: 'https://app.mymagik.com/',
+        url: 'https://app.mymagik.com/saralpay/send-otp',
         success: function(data) {
         	console.log('html' + data);
         	 
